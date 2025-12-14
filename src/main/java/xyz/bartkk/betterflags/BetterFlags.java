72c4a10c04ac1ee4a11d474372bfaff376703d38
Bartkk <bartkk@bartkk.xyz>
2025-12-14 07:40:25 +0100

    Remove unnecessary mixin

--- src/main/java/xyz/bartkk/betterflags/BetterFlags.java
+++ src/main/java/xyz/bartkk/betterflags/BetterFlags.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.render.texture.stitcher.TextureRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//import turniplabs.halplibe.helper.TextureHelper;
+import turniplabs.halplibe.helper.TextureHelper;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
 
 public class BetterFlags implements ModInitializer, ClientStartEntrypoint {
@@ -19,9 +19,7 @@ public class BetterFlags implements ModInitializer, ClientStartEntrypoint {
 	@Override
 	public void beforeClientStart() {
 		try {
-//			TextureRegistry.guiSpriteAtlas.init();
-			TextureRegistry.initializeAllFiles(MOD_ID, TextureRegistry.guiSpriteAtlas,true);
-//			TextureHelper.initializeAllFiles(MOD_ID, TextureRegistry.guiSpriteAtlas,3);
+			TextureHelper.initializeAllFiles(MOD_ID, TextureRegistry.guiSpriteAtlas,3);
 
 		} catch (Exception e) {
 			throw new RuntimeException(e);
